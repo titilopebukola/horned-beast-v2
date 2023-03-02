c3c06e0cca1b67c136aabb9d22a81f6924797600
titilopebukola <titilopebukola@gmail.com>
2023-03-02 10:39:40 +0000

    Add votes

--- src/Component/HornedBeast.js
+++ src/Component/HornedBeast.js
@@ -1,10 +1,15 @@
-import React from "react";
+import { useState } from "react";
 
 export default function HornedBeast({ title, imageUrl, description, keyword, horns }) {
+  const [votes, setVotes] = useState(0);
+
+  function handleVote() {
+    setVotes(votes + 1);
+  }
   return (
     <div className="horned-beast">
       <h2>{title}</h2>
-      <img src={imageUrl} alt="" />
+      <img src={imageUrl} alt="" onClick={handleVote} />
       <p>{description}</p>
       <p>{keyword}</p>
       <p>{horns}</p>
